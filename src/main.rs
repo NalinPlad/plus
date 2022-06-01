20002f4eb1191d4d1c654cfd1ebe24222264fee7
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-06-01 13:00:39 -0700

    exit, quit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 extern crate lalrpop_util;
 extern crate colored;
+extern crate exitcode;
 
 use colored::*;
+use core::f64::consts::E;
+use core::f64::consts::PI;
 use std::collections::HashMap;
 use std::io::BufRead;
 use std::io::Write;
-use core::f64::consts::PI;
-use core::f64::consts::E;
+use std::process;
 
 lalrpop_mod!(pub parser);
 
@@ -15,23 +17,27 @@ fn main() {
     let mut symtab = HashMap::new();
 
     // Insert some default variables. Change these later
-    symtab.insert(String::from("pi"),PI);
-    symtab.insert(String::from("e"),E);
-    symtab.insert(String::from("ans"),0.0_f64); // ans variable. updated after very succseful operation
+    symtab.insert(String::from("pi"), PI);
+    symtab.insert(String::from("e"), E);
+    symtab.insert(String::from("ans"), 0.0_f64); // ans variable. updated after very succseful operation
 
-    println!("{}","rcalc FOSS [HELP for more info]".blue().underline());
-    print!("{} ",">>>>>".blue().bold());
+    println!("{}", "rcalc FOSS [HELP for more info]".blue().underline());
+    print!("{} ", ">>>>>".blue().bold());
     std::io::stdout().flush().unwrap();
     for line in std::io::stdin().lock().lines() {
         let line = line.expect("Input Error");
-        match parser::StatementParser::new().parse(&mut symtab, line.trim()) {
-            Ok(v) => {
-                println!("{}", (v * 10000000000.0).round() / 10000000000.0); // 10 digits of decimal precision
-                *symtab.get_mut("ans").unwrap() = v;
-            },
-            Err(e) => println!("Error : {}", e),
+        if line.trim() == "quit" || line.trim == "exit" {
+            process::exit(exitcode::OK)
+        } else {
+            match parser::StatementParser::new().parse(&mut symtab, line.trim()) {
+                Ok(v) => {
+                    println!("{}", (v * 10000000000.0).round() / 10000000000.0); // 10 digits of decimal precision
+                    *symtab.get_mut("ans").unwrap() = v;
+                }
+                Err(e) => println!("Error : {}", e),
+            }
         }
-        print!("{} ",">>>>>".blue().bold());
+        print!("{} ", ">>>>>".blue().bold());
         std::io::stdout().flush().unwrap();
     }
 }
