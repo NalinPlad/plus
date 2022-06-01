368382828f422e0ad8ad525da02fbdb4bc7da0d1
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-06-01 12:41:02 -0700

    no floating point uncertainty errors?

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use colored::*;
 use std::collections::HashMap;
 use std::io::BufRead;
 use std::io::Write;
+use core::f64::consts::PI;
+use core::f64::consts::E;
 
 lalrpop_mod!(pub parser);
 
@@ -13,8 +15,8 @@ fn main() {
     let mut symtab = HashMap::new();
 
     // Insert some default variables. Change these later
-    symtab.insert(String::from("pi"),3.141_f64);
-    symtab.insert(String::from("e"),2.718_f64);
+    symtab.insert(String::from("pi"),PI);
+    symtab.insert(String::from("e"),E);
     symtab.insert(String::from("ans"),0.0_f64); // ans variable. updated after very succseful operation
 
     println!("{}","rcalc FOSS [HELP for more info]".blue().underline());
@@ -24,7 +26,7 @@ fn main() {
         let line = line.expect("Input Error");
         match parser::StatementParser::new().parse(&mut symtab, line.trim()) {
             Ok(v) => {
-                println!("{}", v);
+                println!("{}", (v * 10000000000.0).round() / 10000000000.0);
                 *symtab.get_mut("ans").unwrap() = v;
             },
             Err(e) => println!("Error : {}", e),
