3331802db70819cb9251f064a30889c3b5aa451a
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-06-01 11:57:48 -0700

    ans var

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,18 @@ fn main() {
     // Insert some default variables. Change these later
     symtab.insert(String::from("pi"),3.141_f64);
     symtab.insert(String::from("e"),2.718_f64);
+    symtab.insert(String::from("ans"),0.0_f64); // ans variable. updated after very succseful operation
+
     println!("{}","rcalc FOSS [HELP for more info]".blue().underline());
     print!("{} ",">>>>>".blue().bold());
     std::io::stdout().flush().unwrap();
     for line in std::io::stdin().lock().lines() {
         let line = line.expect("Input Error");
         match parser::StatementParser::new().parse(&mut symtab, line.trim()) {
-            Ok(v) => println!("{}", v),
+            Ok(v) => {
+                println!("{}", v);
+                *symtab.get_mut("ans").unwrap() = v;
+            },
             Err(e) => println!("Error : {}", e),
         }
         print!("{} ",">>>>>".blue().bold());
